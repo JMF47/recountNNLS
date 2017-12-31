86db7c2c714ecbd19ee95981d7c6a5b0f9d7502e
Jack Fu <jmfu@jhsph.edu>
2017-12-31 00:18:56 -0500

    namespace + imports

--- R/processReadLength.R
+++ R/processReadLength.R
@@ -63,7 +63,7 @@ processReadLength = function(rl, pheno, counts_ex, counts_jx, cores){
       rownames(reads) = NULL; colnames(reads) = pheno$run
       assays = list(counts=reads, se=se)
       data(tx_info, package='recountNNLSdata')
-      rowData(rse_tx) = tx_info[match(rownames(rse_tx), tx_info$tx_name),]
+      SummarizedExperiment::rowData(rse_tx) = tx_info[match(rownames(rse_tx), tx_info$tx_name),]
       rse_tx = SummarizedExperiment::SummarizedExperiment(assays=list(counts=reads, se=se), rowRanges=tx_grl, colData=pheno)
       return(rse_tx)
 }
